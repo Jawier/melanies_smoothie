a34ffd26e873e4607c9efd216612318a9d34bbe7
Jawier <JawierAndrade@Gmail.com>
2024-12-17 18:01:18 +0100

    Update streamlit_app.py
    modified to search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 
 name_on_order =  st.text_input("Name on Smoothie:")
 st.write("The on your Smoothie will be: ", name_on_order)
